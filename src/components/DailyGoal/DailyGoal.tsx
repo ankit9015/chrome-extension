ccdc18e6050e9517d4bcf5cc53a943879dcb2450
Ankit Joshi <67504902+ankit9015@users.noreply.github.com>
2022-08-17 09:49:21 +0530

    fix: error

--- src/components/DailyGoal/DailyGoal.tsx
+++ src/components/DailyGoal/DailyGoal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { GoalState } from "./DailyGoal.types";
 import "./DailyGoal.css";
 import { useBrowser } from "../../context";
